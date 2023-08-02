e81b3d8d538a39b6759f43678d8d4dd56faf525a
DetectiveJeremy <jeremy.r.herrera.ortiz@gmail.com>
2023-08-02 09:39:01 -0400

    hi

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // get references
-
+const btn = document.getElementById("submit-me")
 // set up onclick for clickButton
-
+btn.addEventListener("click", e =>{
+e.preventDefault(); 
+const inputElement = document.getElementById("boxtext");
+const userInput = inputElement.value;
+alert(userInput);
+inputElement.value = ""; 
+} );
 // prevent the page from reloading
 
 // capture user input
